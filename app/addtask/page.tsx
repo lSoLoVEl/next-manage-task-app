c1c9ae4e7afff035a31b981a377d99a666789289
lSoLoVEl <s6519410044@sau.ac.th>
2026-03-17 12:34:49 +0700

    v2:addtaskAndDelShowalltask

--- app/addtask/page.tsx
+++ app/addtask/page.tsx
@@ -1,7 +1,165 @@
+"use client"
+ 
+import logoimg from "@/assets/logo.png";
+import FooterSAU from "../components/FooterSAU";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { supabase } from "@/services/supabaseClient";
+import { useRouter } from "next/navigation";
+ 
 export default function Page() {
-    return (
-        <div>
-            Enter
+  const router = useRouter();
+ 
+  //สร้าง state เพื่อจัดการกับข้อมูลใน component
+  const [title, setTitle] = useState<string>('');
+  const [detail, setDetail] = useState<string>('');
+  const [imageFile, setImageFile] = useState<File | null>(null); //เก็บตัวรูปที่เลือก
+  const [imagePreview, setImagePreview] = useState<string | null>(null); //เก็บที่อยู่ของรูปที่จะแสดง
+  const [isCompleted, setIsCompleted] = useState<boolean>(false);
+ 
+  //ฟังก์ชันเลือกรูป และแสดงรูป
+  const handleSelectImage = (e: React.ChangeEvent<HTMLInputElement>)=>{      
+      const file = e.target.files?.[0]; //e.target.files?.[0] คือ รูปที่เลือก
+ 
+      if(file){
+        setImageFile(file); //เอารูปกำหนดให้กับ imaageFile
+        setImagePreview(URL.createObjectURL(file)); //เอาตำแหน่งรูปกำหนดให้กับ imagePreview
+      }
+  }
+ 
+  //ฟังก์ชันบันทึกข้อมูลเพื่อส่งรูปไป storage:bucket(task_tb) และบันทึกข้อมูลไปยัง database:table(task_tb)
+  const handleSaveClick = async()=>{
+   
+    //Validate UI
+    if(title === '' || detail === '' || imageFile === null){
+        Swal.fire({
+            title:'คำเตือน',
+            text:'กรุณาตรวจสอบการป้อนข้อมูล และเลือกรูปภาพ',
+            icon:'warning',
+            confirmButtonText:'ตกลง',            
+        })
+        return;
+    }
+ 
+    //อัปโหลดรูปไปยัง storage:bucket(task_tb) และ Get image url ของรูปเพื่อไปใช้บันทึกลง database
+    let  image_url = ''; //ตัวแปรเก็บที่อยู่รูป
+   
+    const new_file_name = `${Date.now()}_${imageFile.name}`; //เปลี่ยนชื่อรูป
+ 
+    //อัปโหลด
+    const {error} = await supabase  
+                        .storage
+                        .from('task_bk')
+                        .upload(new_file_name, imageFile);
+    //ไปเอา image url ของรูปที่อัปโหลด
+    const { data } = await supabase
+                        .storage
+                        .from('task_bk')
+                        .getPublicUrl(new_file_name);
+ 
+    image_url = data.publicUrl;
+ 
+    //บันทึกข้อมูลไปยัง database:table(task_tb)
+    const {error: error1} = await supabase
+                        .from('task_tb')
+                        .insert({
+                            title: title,
+                            detail: detail,
+                            image_url: image_url,
+                            is_completed: isCompleted
+                        })
+   
+    if(error1){
+        Swal.fire({
+            title:'เกิดข้อผิดพลาด', text:'กรุณาลองใหม่อีกครั้ง', icon:'error', confirmButtonText:'ตกลง',            
+        })
+        return;
+    }
+ 
+    Swal.fire({
+            title:'ผลการทำงาน', text:'บันทึกข้อมูลเรียบร้อย', icon:'success', confirmButtonText:'ตกลง',          
+        })
+       
+    //แล้วย้อนกลับไปหน้า /showalltask
+    router.push('/showalltask');
+  }
+ 
+ 
+  return (
+    <>
+      <div className="w-3/5 mt-20 p-10 shadow-xl mx-auto mb-10
+                      border border-gray-400 rounded-xl
+                      flex flex-col justify-center items-center">
+        {/* แสดงรูปจาก assets ในโปรเจ็กต์ */}
+        <Image src={logoimg} alt="logo" width={100} height={100} />
+ 
+        {/* แสดงชื่อแอปฯ + การทำงาน */}
+        <h1 className="mt-5 text-2xl font-bold text-gray-700">
+          Manage Task App
+        </h1>
+        <h1 className="mt-3 text-lg text-gray-700">
+            เพิ่มงาน
+        </h1>
+ 
+        {/* ส่วนของการป้อนงาน และรายละเอียดงาน */}
+        <div className="w-full flex flex-col mt-5">
+            <h1>ชื่องาน</h1>
+            <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}
+                   className="p-2 border border-gray-700 rounded mt-1 mb-2"/>
+            <h1>รายละเอียดงาน</h1>
+            <textarea
+                value={detail} onChange={(e)=>setDetail(e.target.value)}
+                className="p-2 border border-gray-700 rounded mt-1 mb-2"
+                rows={4}>
+            </textarea>
         </div>
-    );
+ 
+        {/* ส่วนเลือกรูป และแสดงรูป */}
+        <div  className="w-full flex flex-col mt-5">
+            <h1>อัปโหลดรูป</h1>
+            <input id="selectImage" type="file" accept="image/*"  
+                   className="hidden" onChange={handleSelectImage}/>
+            <label htmlFor="selectImage"
+                   className="px-4 py-2 bg-blue-600 text-white
+                              rounded mt-1 mb-2 w-30 text-center">
+                เลือกรูป
+            </label>
+            {/* แสดงรูป */}
+            {
+                //ตรวจสอบว่ามีการเลือกรูปหรือไม่จาก imagePreview
+                imagePreview &&
+                <Image src={imagePreview} alt="preview" width={150} height={150}/>
+            }
+        </div>
+ 
+        {/* ส่วนเลือกสถานะงาน */}
+        <div className="w-full flex flex-col mt-5">
+            <h1>สถานะงาน</h1>
+            <select value={isCompleted==true?"1":"0"}
+                    onChange={(e)=>setIsCompleted(e.target.value === "1")}
+                    className="p-2 border border-gray-700 rounded mt-1 mb-2">
+                <option value="1">✅ เสร็จแล้ว</option>
+                <option value="0">❌ ยังไม่เสร็จ</option>
+            </select>
+        </div>
+ 
+        {/* ส่วนปุ่มบันทึกงานใหม่ */}
+        <button onClick={handleSaveClick}
+                className="w-full px-4 py-2 bg-green-500 text-white
+                        hover:bg-green-600 rounded mt-5 cursor-pointer">
+            บันทึกงานใหม่
+        </button>
+ 
+        {/* ส่วนปุ่มกลับไปหน้า /showalltask */}
+        <Link href={'/showalltask'} className="mt-3 text-blue-500" >
+            กลับไปหน้าแสดงงานทั้งหมด
+        </Link>
+      </div>
+ 
+      {/* ส่วนของ Footer */}
+      <FooterSAU />
+    </>
+  );
 }
--- app/showalltask/page.tsx
+++ app/showalltask/page.tsx
@@ -1,105 +1,177 @@
-"use client"
+"use client";
+ 
 import Image from "next/image";
-import logoimg from "@/assets/logo.png"
+import logoimg from "@/assets/logo.png";
 import Link from "next/link";
 import FooterSAU from "../components/FooterSAU";
+import { useEffect, useState } from "react";
 import { supabase } from "@/services/supabaseClient";
-
-import {useEffect, useState} from "react";
 import Swal from "sweetalert2";
-// สร้าง interface สำหรับการจัดการข้อมูลงานที่ได้จากฐานข้อมูล
-interface Task{
-    id: number;
-    created_at: string;
-    title: string;
-    detail: string;
-    image_url: string;
-    is_completed: boolean;
-    updated_at: string;
+ 
+// สร้าง interface ที่ล้อกับ colummn ของตารางที่จะทำงานด้วย
+interface Task {
+  id: string;
+  created_at: string;
+  title: string;
+  detail: string;
+  image_url: string;
+  is_completed: boolean;
+  update_at: string;
 }
+ 
 export default function Page() {
-    // สร้าง state สำหรับเก็บข้อมูลงานทั้งหมดที่ได้จากฐานข้อมูล
-    const [tasks, setTasks] = useState<Task[]>([]);
-    // ดึงข้อมูลตอนที่ Component render
-    useEffect(() => {
-        // ฟังก์ชันสำหรับดึงข้อมูลงานทั้งหมดจากฐานข้อมูล
-        const fetchTasks = async () => {
-            // ดึงข้อมูล
-            const {data, error} = await supabase.from("task_tb").select("*").order("created_at", {ascending: false});
-            // ตรวจสอบ Error
-            if ( error ) {
+  //สร้าง state แบบ Task เพื่อเก็บข้อมูลที่ดึงมาจาก supabase
+  const [tasks, setTasks] = useState<Task[]>([]);
+ 
+  //ดึงข้อมูลตอนที่ Component นี้ Render
+  useEffect(() => {
+    //ฟังก์ชันดึงข้อมูล
+    const fetchTasks = async () => {
+      //ดึงข้อมูล
+      const { data, error } = await supabase
+        .from("task_tb")
+        .select("*")
+        .order("created_at", { ascending: false });
+ 
+      //ตรวจสอบ Error
+      if (error) {
+        Swal.fire({
+          icon: "warning",
+          title: "เกิดข้อผิดพลาด",
+          text: "กรุณาลองใหม่อีกครั้ง",
+        });
+        return;
+      }
+ 
+      //ไม่มี Error เอาข้อมูลที่ดึงกำหนดให้กับ state ที่สร้างไว้
+      if (data) {
+        setTasks(data as Task[]);
+      }
+    };
+ 
+    //เรียกใช้ฟัง์ชันดึงข้อมูล
+    fetchTasks();
+  }, []);
+ 
+  //ฟังก์ชันลบ
+  const handleDeleteClick = async (id: string, image_url: string) => {
+    //ยืนยันการลบ
+    Swal.fire({
+      title: "ยืนยันการลบ",    
+      text: "คุณต้องการลบข้อมูลนี้หรือไม่",
+      icon: "question",
+      confirmButtonText: "ยืนยัน",
+      showCancelButton: true,
+      cancelButtonText: "ยกเลิก",
+    }).then(async (result) => {
+      if(result.isConfirmed){
+        //ลบข้อมูลใน database
+        const { error: error1 } = await supabase
+          .from("task_tb")
+          .delete()
+          .eq("id", id);
+       
+        if(error1){
+            Swal.fire({
+                title:'เกิดข้อผิดพลาด', text:'ไม่สามารถลบข้อมูลในฐานข้อมูลได้ กรุณาลองใหม่อีกครั้ง', icon:'error', confirmButtonText:'ตกลง',            
+            })
+            return;
+        }
+ 
+        //ลบรูปจาก storage
+        const { error: error2 } = await supabase
+          .storage
+          .from("task_bk")
+          .remove([image_url.substring(image_url.lastIndexOf("/") + 1)]);
+ 
+        if(error2){
                 Swal.fire({
-                    icon: "Warning",
-                    title: "เกิดผิดพลาด",
-                    text: "ไม่สามารถดึงข้อมูลได้ กรุณาลองใหม่อีกครั้ง",
+                    title:'เกิดข้อผิดพลาด', text:'ไม่สามารถลบรูปใน storage ได้ กรุณาลองใหม่อีกครั้ง', icon:'error', confirmButtonText:'ตกลง',            
                 })
                 return;
-            } 
-                // ไม่มี Error นำข้อมูลที่ได้มาเก็บใน State
-               if(data) {
-                setTasks(data);
-            }
         }
-        // เรียกใช้ฟังก์ชันดึงข้อมูล
-        fetchTasks();
-    }, []);
-    return (
-        <>
-        <div
+ 
+        //อัปเดจข้อมูลในตาราง
+        setTasks(tasks.filter((task) => task.id !== id));
+      }
+    })
+  }
+ 
+  return (
+    <>
+      <div
         className="w-3/5 mt-20 p-10 shadow-xl mx-auto
                       border border-gray-400 rounded-xl
-                      flex flex-col justify-center items-center">
-                        {/* รูปจาก assets */}
+                      flex flex-col justify-center items-center"
+      >
+        {/* แสดงรูปจาก assets ในโปรเจ็กต์ */}
         <Image src={logoimg} alt="logo" width={100} height={100} />
-        {/* แสดงชื่อแอปฯ การทำงาน */}
+ 
+        {/* แสดงชื่อแอปฯ + การทำงาน */}
         <h1 className="mt-5 text-2xl font-bold text-gray-700">
           Manage Task App
         </h1>
-        <h1 className="mt-3 text-lg text-gray-700">
-          ข้อมูลงานทั้งหมด
-        </h1>
-        {/* แสดงปุ่มเพิ่มงาน */}
+        <h1 className="mt-3 text-lg text-gray-700">ข้อมูลงานทั้งหมด</h1>
+ 
+        {/* แสดงปุ่มเพิ่มงาน  */}
         <div className="w-full mt-5 flex justify-end">
-            <Link href="/addtask">
-              <button 
-              className=" bg-blue-600 py-2 px-5 text-white rounded hover:bg-blue-700 cursor-pointer">
-                เพิ่มงาน
-              </button>
-            </Link>
+          <Link
+            href="/addtask"
+            className="bg-blue-600 py-2 px-5 rounded
+                             hover:bg-blue-800 text-white"
+          >
+            เพิ่มงาน
+          </Link>
         </div>
+ 
         {/* แสดงตารางที่นำข้อมูลทั้งหมดจาก task_tb มาแสดง */}
-        <table className="w-full mt-5 border border-gray-400">
+        <table className="w-full border border-gray-500 mt-5">
           <thead>
             <tr className="bg-gray-200">
-              <th className="border border-gray-400 p-2">รูปงาน</th>
-              <th className="border border-gray-400 p-2">ชื่องานที่ทำ</th>
-              <th className="border border-gray-400 p-2">รายละเอียดงาน</th>
-              <th className="border border-gray-400 p-2">สถานะ</th>
-            <th className="border border-gray-400 p-2">วันที่เพิ่ม</th>
-            <th className="border border-gray-400 p-2">วันที่แก้ไข</th>
-            <th className="border border-gray-400 p-2">Action</th>
-                
+              <th className="border border-gray-500 p-2">รูปงาน</th>
+              <th className="border border-gray-500 p-2">งานที่ทำ</th>
+              <th className="border border-gray-500 p-2">รายละเอียดงาน</th>
+              <th className="border border-gray-500 p-2">สถานะ</th>
+              <th className="border border-gray-500 p-2">วันที่เพิ่ม</th>
+              <th className="border border-gray-500 p-2">วันที่แก้ไข</th>
+              <th className="border border-gray-500 p-2">Action</th>
             </tr>
           </thead>
-            <tbody>
-                {
-                    tasks.map((item) => (
-                        <tr key={item.id} className="bg-gray-200">
-              <td className="border border-gray-400 p-2 "><Image src={item.image_url} alt={item.title} width={50} height={50} className="mx-auto" /></td>
-              <td className="border border-gray-400 p-2">{item.title}</td>
-              <td className="border border-gray-400 p-2">{item.detail}</td>
-              <td className="border border-gray-400 p-2">{item.is_completed ? "เสร็จสิ้น" : "ยังไม่เสร็จ"}</td>
-            <td className="border border-gray-400 p-2 text-center">{new Date(item.created_at).toLocaleDateString('th-TH')}</td>
-            <td className="border border-gray-400 p-2 text-center">{new Date(item.updated_at).toLocaleDateString()}</td>
-            <td className="border border-gray-400 p-2 flex justify-center ">แก้ไขหรือลบ</td>
-                
-            </tr>
-                    ))
-                }
-                </tbody>
+          <tbody>
+            {tasks.map((item) => (
+              <tr key={item.id}>
+                <td className="border border-gray-500 p-2">
+                    <Image src={item.image_url} alt="logo" width={50} height={50} className="mx-auto"/>
+                </td>
+                <td className="border border-gray-500 p-2">{item.title}</td>
+                <td className="border border-gray-500 p-2">{item.detail}</td>
+                <td className="border border-gray-500 p-2">
+                  {item.is_completed ? "เสร็จสิ้น" : "ยังไม่เสร็จสิ้น"}
+                </td>
+                <td className="border border-gray-500 p-2 text-center">
+                  {new Date(item.created_at).toLocaleString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}
+                </td>
+                <td className="border border-gray-500 p-2 text-center">
+                  {new Date(item.update_at).toLocaleString('th-TH', { year: 'numeric', month: 'long', day: 'numeric' })}
+                </td>
+                <td className="border border-gray-500 p-2 text-center">
+                    <Link href={`/edittask/${item.id}`} className="text-green-500">
+                        แก้ไข
+                    </Link>                    
+                    {' '}|{' '}
+                    <button className="cursor-pointer text-red-500"
+                            onClick={ ()=>handleDeleteClick(item.id, item.image_url) } >
+                        ลบ  
+                    </button>                    
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
-      <FooterSAU/>
-        </>
-    );
+ 
+      {/* แสดง Footer */}
+      <FooterSAU />
+    </>
+  );
 }
